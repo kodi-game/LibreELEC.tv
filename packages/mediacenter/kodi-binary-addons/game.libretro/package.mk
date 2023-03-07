a3c4e919f06397035efbc39147a34df0625d2ab8
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-06 17:23:10 -0800

    kodi: Update to retroplayer branch

--- packages/mediacenter/kodi-binary-addons/game.libretro/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro/package.mk
@@ -2,8 +2,7 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.libretro"
-PKG_VERSION="21.0.2-Omega"
-PKG_SHA256="995fe54a5b5b296cd43d5029efd445fbb467c4623ecb9f21bd20f96d123b6b57"
+PKG_VERSION="7fa133064b95ffcbc49b5c43ac62cdd7b9c34a9b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
