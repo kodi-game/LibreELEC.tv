d2a7421125cddd9997c7b1733ff7f5efadffb7bd
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 15:07:00 -0700

    Add Lakka libretro cores and dependencies

---
+++ packages/lakka/libretro_cores/beetle_lynx/package.mk
@@ -0,0 +1,13 @@
+PKG_NAME="beetle_lynx"
+PKG_VERSION="d718564b468590f6ed8abf5de6d2e9dfab64cb53"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/beetle-lynx-libretro"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="libretro implementation of Mednafen Lynx"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v mednafen_lynx_libretro.so ${INSTALL}/usr/lib/libretro/
+}
---
+++ packages/lakka/libretro_cores/kronos/package.mk
@@ -0,0 +1,40 @@
+PKG_NAME="kronos"
+PKG_VERSION="fec6e18cc6f00933f6303a983935a44302f3075f"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/yabause"
+PKG_URL="${PKG_SITE}.git"
+PKG_GIT_CLONE_BRANCH="kronos"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Port of Kronos to libretro."
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C yabause/src/libretro HAVE_CDROM=1"
+
+if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  PKG_MAKE_OPTS_TARGET+=" HAVE_OPENGL=1"
+fi
+
+if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  PKG_MAKE_OPTS_TARGET+=" FORCE_GLES=1"
+fi
+
+if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${VULKAN}"
+fi
+
+if [ "${ARCH}" = "arm" ]; then
+  if [ "${PROJECT}" = "Samsung" -a "${DEVICE}" = "Exynos" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=odroid BOARD=ODROID-XU4"
+  else
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+  fi
+elif [ "${ARCH}" = "aarch64" ]; then
+  PKG_MAKE_OPTS_TARGET+=" platform=arm64"
+fi
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v yabause/src/libretro/kronos_libretro.so ${INSTALL}/usr/lib/libretro/
+}
