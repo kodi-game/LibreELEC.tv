21d5e005251eeb88271b340ff590460b3d64cfdb
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/cap32/package.mk
+++ packages/lakka/libretro_cores/cap32/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="cap32"
-PKG_VERSION="22d0475be7f6249669358d02b6cba4bc98abe936"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/libretro-cap32"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="caprice32 4.2.0 libretro"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-cap32"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v cap32_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/cap32_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/vecx/package.mk
+++ packages/lakka/libretro_cores/vecx/package.mk
@@ -1,37 +1,8 @@
 PKG_NAME="vecx"
-PKG_VERSION="33a8a89e92dbcef047a12e2c02dd55878be02a8f"
-PKG_LICENSE="GPLv2|LGPLv2.1"
-PKG_SITE="https://github.com/libretro/libretro-vecx"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="libretro adaptation of vecx"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
-
-if [ "${OPENGL_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGL}"
-fi
-
-if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
-fi
-
-if [ "${VULKAN_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${VULKAN}"
-fi
-
-if [ "${PROJECT}" = "RPi" ]; then
-  if [ "${OPENGLES}" = "mesa" ]; then
-    PKG_MAKE_OPTS_TARGET+=" platform=rpi-mesa"
-  else
-    PKG_MAKE_OPTS_TARGET+=" platform=rpi"
-  fi
-elif [ "${OPENGLES_SUPPORT}" = "yes" ]; then
-  PKG_MAKE_OPTS_TARGET+=" HAS_GLES=1 GLES=1"
-fi
+PKG_DEPENDS_TARGET="libretro-vecx"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v vecx_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/vecx_libretro.so ${INSTALL}/usr/lib/libretro/
 }
