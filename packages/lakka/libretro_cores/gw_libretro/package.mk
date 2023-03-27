e242cf9a9d5e2cc1fb8c70d1694be164325be2b8
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/gw_libretro/package.mk
+++ packages/lakka/libretro_cores/gw_libretro/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="gw_libretro"
-PKG_VERSION="19a1cb3105ca4a82139fb4994e7995fd956f6f8d"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/gw-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="A libretro core for Game & Watch simulators"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_DEPENDS_TARGET="libretro-gw"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v gw_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/gw_libretro.so ${INSTALL}/usr/lib/libretro/
 }
