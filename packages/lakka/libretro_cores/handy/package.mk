9f6f89bb24059c2239dfbc641e6e19f1c50e7b95
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/handy/package.mk
+++ packages/lakka/libretro_cores/handy/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="handy"
-PKG_VERSION="7c2dbcb46a26cdb2355ccb4e982460095a55ba44"
-PKG_LICENSE="Zlib"
-PKG_SITE="https://github.com/libretro/libretro-handy"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="K. Wilkins' Atari Lynx emulator Handy for libretro"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-handy"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v handy_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/handy_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/nestopia/package.mk
+++ packages/lakka/libretro_cores/nestopia/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="nestopia"
-PKG_VERSION="125e2ab6d7f2dd4accc60cb84154adad21d3a6a9"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/nestopia"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Libretro implementation of NEStopia. (Nintendo Entertainment System)"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-C libretro/"
+PKG_DEPENDS_TARGET="libretro-nestopia"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v libretro/nestopia_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/nestopia_libretro.so ${INSTALL}/usr/lib/libretro/
 }
