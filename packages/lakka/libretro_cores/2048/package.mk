44aa72d5cfed0b9c0fc6f020dc4f645f392c83b6
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/2048/package.mk
+++ packages/lakka/libretro_cores/2048/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="2048"
-PKG_VERSION="0edef01f4c2dd2cbd0bb66b888ca4b57fad297d1"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/libretro-2048"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Port of 2048 puzzle game to the libretro API."
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_DEPENDS_TARGET="libretro-2048"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v 2048_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/2048_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/opera/package.mk
+++ packages/lakka/libretro_cores/opera/package.mk
@@ -1,17 +1,8 @@
 PKG_NAME="opera"
-PKG_VERSION="8a49bb8877611037438aeb857cb182f41ee0e3a1"
-PKG_LICENSE="LGPL with additional notes"
-PKG_SITE="https://github.com/libretro/opera-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Port of 4DO/libfreedo to libretro."
-PKG_TOOLCHAIN="make"
-
-pre_make_target() {
-  PKG_MAKE_OPTS_TARGET="CC=${CC} CXX=${CXX} AR=${AR}"
-}
+PKG_DEPENDS_TARGET="libretro-opera"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v opera_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/opera_libretro.so ${INSTALL}/usr/lib/libretro/
 }
