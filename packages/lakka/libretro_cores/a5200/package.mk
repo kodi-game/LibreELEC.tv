bc703fb3b5a037ea2d3babc6c003420c5f7d69b9
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/a5200/package.mk
+++ packages/lakka/libretro_cores/a5200/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="a5200"
-PKG_VERSION="6ae27c21c0d7d941d14c25f7d5d3da5215ddd9c8"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/libretro/a5200"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Libretro port of Atari 5200 emulator version 2.0.2 for GCW0"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-a5200"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v a5200_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/a5200_libretro.so ${INSTALL}/usr/lib/libretro/
 }
