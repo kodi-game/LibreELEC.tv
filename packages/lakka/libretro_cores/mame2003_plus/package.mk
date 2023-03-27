e92702f114913646d3a7937fd0740eae6f03c21a
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/mame2003_plus/package.mk
+++ packages/lakka/libretro_cores/mame2003_plus/package.mk
@@ -1,26 +1,14 @@
 PKG_NAME="mame2003_plus"
-PKG_VERSION="2bd4ba1be083e9a4a79cb8550444470fd6512fc1"
-PKG_LICENSE="MAME"
-PKG_SITE="https://github.com/libretro/mame2003-plus-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="MAME - Multiple Arcade Machine Emulator"
-PKG_TOOLCHAIN="make"
-
-pre_make_target() {
-  PKG_MAKE_OPTS_TARGET="ARCH= CC=${CC} NATIVE_CC=${CC} LD=${CC}"
-}
+PKG_DEPENDS_TARGET="libretro-mame2003_plus"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v mame2003_plus_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mame2003_plus_libretro.so ${INSTALL}/usr/lib/libretro/
 
   mkdir -p ${INSTALL}/usr/share/libretro-database/mame2003-plus
-    cp -v metadata/mame2003-plus.xml ${INSTALL}/usr/share/libretro-database/mame2003-plus
+  cp -rv ${PKG_ORIG_SYSROOT_PREFIX}/usr/share/libretro-database/mame2003-plus/* ${INSTALL}/usr/share/libretro-database/mame2003-plus
 
   mkdir -p ${INSTALL}/usr/share/retroarch/system/mame2003-plus/samples
-    cp -rv metadata/artwork ${INSTALL}/usr/share/retroarch/system/mame2003-plus
-    cp -v metadata/{cheat,hiscore,history}.dat ${INSTALL}/usr/share/retroarch/system/mame2003-plus
-    # something must be in a folder in order to include it in the image, so why not some instructions
-    echo "Put your samples here." > ${INSTALL}/usr/share/retroarch/system/mame2003-plus/samples/readme.txt
+  cp -rv ${PKG_ORIG_SYSROOT_PREFIX}/usr/share/retroarch/system/mame2003-plus/samples/* ${INSTALL}/usr/share/retroarch/system/mame2003-plus
 }
