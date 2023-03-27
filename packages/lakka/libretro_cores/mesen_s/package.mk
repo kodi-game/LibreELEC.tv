7280e977d2bbd0956775d877c481d2a58920c1a5
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/mesen_s/package.mk
+++ packages/lakka/libretro_cores/mesen_s/package.mk
@@ -1,16 +1,8 @@
 PKG_NAME="mesen_s"
-PKG_VERSION="32a7adfb4edb029324253cb3632dfc6599ad1aa8"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/Mesen-S"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Mesen-S is a cross-platform (Windows & Linux) SNES emulator built in C++ and C#"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-C Libretro/"
+PKG_DEPENDS_TARGET="libretro-mesen-s"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v Libretro/mesen-s_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mesen-s_libretro.so ${INSTALL}/usr/lib/libretro/
 }
-
