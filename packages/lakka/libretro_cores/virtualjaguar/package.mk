4af766b7134daee40a954dd6f09120aeb2485472
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/virtualjaguar/package.mk
+++ packages/lakka/libretro_cores/virtualjaguar/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="virtualjaguar"
-PKG_VERSION="2cc06899b839639397b8b30384a191424b6f529d"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/virtualjaguar-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Port of Virtual Jaguar to Libretro"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-virtualjaguar"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v virtualjaguar_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/virtualjaguar_libretro.so ${INSTALL}/usr/lib/libretro/
 }
