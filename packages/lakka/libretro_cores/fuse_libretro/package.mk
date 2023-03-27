d7ac67dd4e9dec2023c8ec4e4bfde0ce08802213
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/fuse_libretro/package.mk
+++ packages/lakka/libretro_cores/fuse_libretro/package.mk
@@ -1,20 +1,8 @@
 PKG_NAME="fuse_libretro"
-PKG_VERSION="3f9344ddf88dfd251d95cffdea615692479e8cc6"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/fuse-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain bzip2"
-PKG_LONGDESC="A port of the Fuse Unix Spectrum Emulator to libretro "
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
-
-pre_make_target() {
-  CFLAGS+=" -DHAVE_LIBBZ2"
-  CXXFLAGS+=" -DHAVE_LIBBZ2"
-}
+PKG_DEPENDS_TARGET="libretro-fuse"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v fuse_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/fuse_libretro.so ${INSTALL}/usr/lib/libretro/
 }
