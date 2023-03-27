e7e9b40c5a62d992af79e66070637bea401eaa7b
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/dosbox_pure/package.mk
+++ packages/lakka/libretro_cores/dosbox_pure/package.mk
@@ -1,20 +1,8 @@
 PKG_NAME="dosbox_pure"
-PKG_VERSION="4fdb557e415698aae5bd90b076f76437f5e9b0b4"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/dosbox-pure"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
-PKG_TOOLCHAIN="make"
-
-make_target() {
-  # remove optimization from CFLAGS, set via Makefile
-  CFLAGS="${CFLAGS//-O3/}"
-  CFLAGS="${CFLAGS//-O2/}"
-  make CXX=${CXX} CPUFLAGS="${CFLAGS}"
-}
+PKG_DEPENDS_TARGET="libretro-dosbox-pure"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v dosbox_pure_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/dosbox_pure_libretro.so ${INSTALL}/usr/lib/libretro/
 }
