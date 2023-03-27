34d22839395799d9e359284bfbbcbe85c12fb397
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/prboom/package.mk
+++ packages/lakka/libretro_cores/prboom/package.mk
@@ -1,15 +1,11 @@
 PKG_NAME="prboom"
-PKG_VERSION="1de4411265c717d787b1d512b7f1529314c9c281"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/libretro-prboom"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="libretro implementation of Doom"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-prboom"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v prboom_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/prboom_libretro.so ${INSTALL}/usr/lib/libretro/
+
   mkdir -p ${INSTALL}/usr/share/retroarch/system/
-    cp -v prboom.wad ${INSTALL}/usr/share/retroarch/system/
+  cp -rv ${PKG_ORIG_SYSROOT_PREFIX}/usr/share/retroarch/system/* ${INSTALL}/usr/share/retroarch/system/
 }
