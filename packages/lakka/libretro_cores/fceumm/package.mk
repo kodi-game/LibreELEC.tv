d5e26876d11d9ba76200c646eda012b5c88c69bb
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/fceumm/package.mk
+++ packages/lakka/libretro_cores/fceumm/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="fceumm"
-PKG_VERSION="729d42572e6b926ec03baf31e3378a7a2ac151ab"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/libretro-fceumm"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Port of FCEUmm / FCEUX to Libretro."
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_DEPENDS_TARGET="libretro-fceumm"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v fceumm_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/fceumm_libretro.so ${INSTALL}/usr/lib/libretro/
 }
