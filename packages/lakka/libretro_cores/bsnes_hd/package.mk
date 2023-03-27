9b7773fa3d1bec103cc3a8b7b80eccfbb7aeacf0
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/bsnes_hd/package.mk
+++ packages/lakka/libretro_cores/bsnes_hd/package.mk
@@ -1,18 +1,8 @@
 PKG_NAME="bsnes_hd"
-PKG_VERSION="04821703aefdc909a4fd66d168433fcac06c2ba7"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/DerKoun/bsnes-hd"
-PKG_URL="${PKG_SITE}.git"
-PKG_BRANCH="master"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Fork of bsnes that adds HD video features."
-PKG_TOOLCHAIN="make"
-
-make_target() {
-  make -C bsnes -f GNUmakefile target="libretro" compiler="${CXX}" CXXFLAGS="${CXXFLAGS}" platform=linux local=false binary=library
-}
+PKG_DEPENDS_TARGET="libretro-bsnes-hd"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v bsnes/out/bsnes_hd_beta_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/bsnes_hd_beta_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/mame2000/package.mk
+++ packages/lakka/libretro_cores/mame2000/package.mk
@@ -1,17 +1,8 @@
 PKG_NAME="mame2000"
-PKG_VERSION="0208517404e841fce0c094f1a2776a0e1c6c101d"
-PKG_LICENSE="MAME"
-PKG_SITE="https://github.com/libretro/mame2000-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="MAME - Multiple Arcade Machine Emulator"
-PKG_TOOLCHAIN="make"
-
-if [ "${ARCH}" = "arm" ]; then
-  PKG_MAKE_OPTS_TARGET="ARM=1 USE_CYCLONE=1 USE_DRZ80=1"
-fi
+PKG_DEPENDS_TARGET="libretro-mame2000"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v mame2000_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mame2000_libretro.so ${INSTALL}/usr/lib/libretro/
 }
