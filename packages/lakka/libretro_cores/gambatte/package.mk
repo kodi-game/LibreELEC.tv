f1b32db1e2b9a1aaf2655c0da2b3a5aefd12868d
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/gambatte/package.mk
+++ packages/lakka/libretro_cores/gambatte/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="gambatte"
-PKG_VERSION="4c64b5285b88a08b8134f6c36177fdca56d46192"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/gambatte-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="libretro implementation of libgambatte, an open-source Game Boy Color emulator written for fun and made available in the hope that it will be useful."
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_DEPENDS_TARGET="libretro-gambatte"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v gambatte_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/gambatte_libretro.so ${INSTALL}/usr/lib/libretro/
 }
