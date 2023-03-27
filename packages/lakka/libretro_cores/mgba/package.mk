b6f20fcff227883a0239c50e3375ff1d597f23e3
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/mgba/package.mk
+++ packages/lakka/libretro_cores/mgba/package.mk
@@ -1,23 +1,8 @@
 PKG_NAME="mgba"
-PKG_VERSION="a69c3434afe8b26cb8f9463077794edfa7d5efad"
-PKG_LICENSE="MPLv2.0"
-PKG_SITE="https://github.com/libretro/mgba"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="mGBA Game Boy Advance Emulator"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-C ../ -f Makefile.libretro"
-
-if [ "${ARCH}" = "arm" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=unix-armv"
-fi
-
-if target_has_feature neon ; then
-  PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
-fi
+PKG_DEPENDS_TARGET="libretro-mgba"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../mgba_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mgba_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/puae/package.mk
+++ packages/lakka/libretro_cores/puae/package.mk
@@ -1,15 +1,11 @@
 PKG_NAME="puae"
-PKG_VERSION="b2c4acb0ae12a507647f8f181962f96ad740f2e8"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/libretro/libretro-uae"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Portable Commodore Amiga Emulator"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-uae"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v puae_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/puae_libretro.so ${INSTALL}/usr/lib/libretro/
+
   mkdir -p ${INSTALL}/usr/share/retroarch/system/uae_data
-    cp -vR ${PKG_BUILD}/sources/uae_data/* ${INSTALL}/usr/share/retroarch/system/uae_data/
+  cp -vR ${PKG_ORIG_SYSROOT_PREFIX}/usr/share/retroarch/system/uae_data/* ${INSTALL}/usr/share/retroarch/system/uae_data/
 }
