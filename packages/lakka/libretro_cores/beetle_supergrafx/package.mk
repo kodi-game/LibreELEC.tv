f5cc837cc4bb3e6cf3b97bf8fa55f4473e7ba49f
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/beetle_supergrafx/package.mk
+++ packages/lakka/libretro_cores/beetle_supergrafx/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="beetle_supergrafx"
-PKG_VERSION="b8ead15fc8d8a97b721e187033f2dcbfad62acc3"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/beetle-supergrafx-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Standalone port of Mednafen PCE Fast to libretro."
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-beetle-supergrafx"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v mednafen_supergrafx_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mednafen_supergrafx_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/mesen/package.mk
+++ packages/lakka/libretro_cores/mesen/package.mk
@@ -1,16 +1,8 @@
 PKG_NAME="mesen"
-PKG_VERSION="c89474c9d87df967d21b7b7d5971dc9475fec028"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/Mesen"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-C Libretro/"
+PKG_DEPENDS_TARGET="libretro-mesen"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v Libretro/mesen_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mesen_libretro.so ${INSTALL}/usr/lib/libretro/
 }
-
