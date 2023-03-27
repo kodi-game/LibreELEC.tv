ff9587a312bd437462854c636ddf6db596e48a67
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/bk_emulator/package.mk
+++ packages/lakka/libretro_cores/bk_emulator/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="bk_emulator"
-PKG_VERSION="cf70ce503ddb65051a58f3c4f61d1e258fc4c84b"
-PKG_LICENSE="Opensource"
-PKG_SITE="https://github.com/libretro/bk-emulator"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC=""
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_DEPENDS_TARGET="libretro-bk"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v bk_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/bk_libretro.so ${INSTALL}/usr/lib/libretro/
 }
--- packages/lakka/libretro_cores/hatari/package.mk
+++ packages/lakka/libretro_cores/hatari/package.mk
@@ -1,21 +1,8 @@
 PKG_NAME="hatari"
-PKG_VERSION="1ebf0a0488580ef95c0b28f02223b31813c867c5"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/hatari"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="New rebasing of Hatari based on Mercurial upstream. Tries to be a shallow fork for easy upstreaming later on."
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-C ../ -f Makefile.libretro"
-
-pre_make_target() {
-  if [ "${ARCH}" = "arm" ]; then
-    CFLAGS+=" -DNO_ASM -DARM -D__arm__ -DARM_ASM -DNOSSE -DARM_HARDFP"
-  fi
-}
+PKG_DEPENDS_TARGET="libretro-hatari"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../hatari_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/hatari_libretro.so ${INSTALL}/usr/lib/libretro/
 }
