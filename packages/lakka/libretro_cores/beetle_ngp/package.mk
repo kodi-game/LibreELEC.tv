bbd0bbf9666e9f438ce1bfec39d304a9356387db
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/beetle_ngp/package.mk
+++ packages/lakka/libretro_cores/beetle_ngp/package.mk
@@ -1,13 +1,8 @@
 PKG_NAME="beetle_ngp"
-PKG_VERSION="00c7cb8ea97ad9a372307405d8abf34e401fec8a"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/beetle-ngp-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="libretro implementation of Mednafen Neo Geo Pocket."
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-beetle-ngp"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v mednafen_ngp_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/mednafen_ngp_libretro.so ${INSTALL}/usr/lib/libretro/
 }
