3e99c33614f4d47d77881f2d3effd83ff376fcc6
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/dosbox/package.mk
+++ packages/lakka/libretro_cores/dosbox/package.mk
@@ -1,19 +1,8 @@
 PKG_NAME="dosbox"
-PKG_VERSION="b7b24262c282c0caef2368c87323ff8c381b3102"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/dosbox-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="libretro wrapper for the DOSBox emulator"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
-
-pre_make_target() {
-  CXXFLAGS+=" -std=gnu++11"
-}
+PKG_DEPENDS_TARGET="libretro-dosbox"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v dosbox_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/dosbox_libretro.so ${INSTALL}/usr/lib/libretro/
 }
