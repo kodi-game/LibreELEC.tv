3c76808cab7a454b0b7132a414796f1e83b550de
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/dinothawr/package.mk
+++ packages/lakka/libretro_cores/dinothawr/package.mk
@@ -1,19 +1,11 @@
 PKG_NAME="dinothawr"
-PKG_VERSION="33fb82a8df4e440f96d19bba38668beaa1b414fc"
-PKG_LICENSE="Non-commercial"
-PKG_SITE="https://github.com/libretro/Dinothawr"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Dinothawr is a block pushing puzzle game on slippery surfaces"
-PKG_TOOLCHAIN="make"
-
-if target_has_feature neon; then
-  PKG_MAKE_OPTS_TARGET="HAVE_NEON=1"
-fi
+PKG_DEPENDS_TARGET="libretro-dinothawr"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v dinothawr_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/dinothawr_libretro.so ${INSTALL}/usr/lib/libretro/
+
   mkdir -p ${INSTALL}/usr/share/retroarch/system
-    cp -rv dinothawr ${INSTALL}/usr/share/retroarch/system/
+  cp -rv ${PKG_ORIG_SYSROOT_PREFIX}/usr/share/retroarch/system/dinothawr ${INSTALL}/usr/share/retroarch/system/
 }
--- packages/lakka/libretro_cores/vbam/package.mk
+++ packages/lakka/libretro_cores/vbam/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="vbam"
-PKG_VERSION="4b605b4b1ad88c1077ede7abdfff03e7f8392f27"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/visualboyadvance-m/visualboyadvance-m"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="VBA-M with libretro integration"
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-C ../src/libretro/"
+PKG_DEPENDS_TARGET="libretro-vbam"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../src/libretro/vbam_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/vbam_libretro.so ${INSTALL}/usr/lib/libretro/
 }
