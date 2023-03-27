a22a1af2cb4d66c9fdeedc275eeba31ee5c5b75e
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/bluemsx/package.mk
+++ packages/lakka/libretro_cores/bluemsx/package.mk
@@ -1,15 +1,8 @@
 PKG_NAME="bluemsx"
-PKG_VERSION="acf358be18644a9df0ed9602d63c2f73d4fe605a"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/blueMSX-libretro"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Port of blueMSX to the libretro API."
-PKG_TOOLCHAIN="make"
-
-PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_DEPENDS_TARGET="libretro-bluemsx"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v bluemsx_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/bluemsx_libretro.so ${INSTALL}/usr/lib/libretro/
 }
