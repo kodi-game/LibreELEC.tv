4d0feb398696f99c960a4a08fd2e9f4d792951b5
Garrett Brown <themagnificentmrb@gmail.com>
2023-03-26 18:07:34 -0700

    Lakka: Use upstream libretro cores

--- packages/lakka/libretro_cores/cannonball/package.mk
+++ packages/lakka/libretro_cores/cannonball/package.mk
@@ -1,16 +1,11 @@
 PKG_NAME="cannonball"
-PKG_VERSION="8fb0d9561ee110f31f45610661649f0c1ff068ee"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/cannonball"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Cannonball: An Enhanced OutRun Engine"
-PKG_TOOLCHAIN="make"
+PKG_DEPENDS_TARGET="libretro-cannonball"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v cannonball_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/cannonball_libretro.so ${INSTALL}/usr/lib/libretro/
+
   mkdir -p ${INSTALL}/usr/share/retroarch/system/cannonball/res/
-    cp -v res/{tilemap.bin,tilepatch.bin} docs/license.txt ${INSTALL}/usr/share/retroarch/system/cannonball/res/
-    cp -v roms/roms.txt ${INSTALL}/usr/share/retroarch/system/cannonball/
+  cp -rv ${PKG_ORIG_SYSROOT_PREFIX}/usr/share/retroarch/system/cannonball ${INSTALL}/usr/share/retroarch/system/cannonball/res/
 }
