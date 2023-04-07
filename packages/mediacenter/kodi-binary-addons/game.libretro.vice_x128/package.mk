02f871dfb0844899787bd60a0118d870e1282789
Garrett Brown <themagnificentmrb@gmail.com>
2023-04-06 19:03:00 -0700

    [temp] Depend on Lakka packages for testing

--- packages/mediacenter/kodi-binary-addons/game.libretro.vice_x128/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.vice_x128/package.mk
@@ -9,7 +9,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/kodi-game/game.libretro.vice_x128"
 PKG_URL="https://github.com/kodi-game/game.libretro.vice_x128/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-vice_x128"
+PKG_DEPENDS_TARGET="toolchain kodi-platform vice"
 PKG_SECTION=""
 PKG_LONGDESC="game.libretro.vice_x128: vice (x128) emulator for Kodi"
 
