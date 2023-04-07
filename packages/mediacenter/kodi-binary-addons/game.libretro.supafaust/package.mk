fde36aa04768672d694ade95afac073793c593d4
Garrett Brown <themagnificentmrb@gmail.com>
2023-04-06 19:03:00 -0700

    [temp] Depend on Lakka packages for testing

--- packages/mediacenter/kodi-binary-addons/game.libretro.supafaust/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.supafaust/package.mk
@@ -9,7 +9,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/kodi-game/game.libretro.supafaust"
 PKG_URL="https://github.com/kodi-game/game.libretro.supafaust/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-supafaust"
+PKG_DEPENDS_TARGET="toolchain kodi-platform beetle_supafaust"
 PKG_SECTION=""
 PKG_LONGDESC="game.libretro.supafaust: Beetle supafaust for Kodi"
 
