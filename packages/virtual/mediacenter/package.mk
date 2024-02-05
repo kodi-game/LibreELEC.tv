b21429a00734f1bcc4acabd0c7b7ea58acc4061d
Garrett Brown <themagnificentmrb@gmail.com>
2024-02-05 12:00:49 -0800

    virtual/mediacenter: Add Atari game add-ons

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -49,6 +49,9 @@ if [ "${MEDIACENTER}" = "kodi" ]; then
   fi
 
   PKG_DEPENDS_TARGET+=" game.libretro"
+  PKG_DEPENDS_TARGET+=" game.libretro.a5200"
+  PKG_DEPENDS_TARGET+=" game.libretro.atari800"
+  PKG_DEPENDS_TARGET+=" game.libretro.prosystem"
   PKG_DEPENDS_TARGET+=" game.shader.presets"
   PKG_DEPENDS_TARGET+=" inputstream.adaptive"
   PKG_DEPENDS_TARGET+=" vfs.libarchive"
